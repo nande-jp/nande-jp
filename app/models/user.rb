9003c87ce6bbc74af1eedaf3dea70a027615f97d
tmouri <takehiromouri@gmail.com>
2019-12-25 06:37:25 -0800

    Add JA translations for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates_presence_of :username
+  validates_uniqueness_of :username
+
   has_many :questions
   has_many :answers
   has_many :bookmarks
