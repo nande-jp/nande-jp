541424e1e5156e7cd16e2689385fed42f3e82ea4
tmouri <takehiromouri@gmail.com>
2020-02-08 17:39:28 -0800

    Add error page

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module NandeJp
 
     config.generators.stylesheets = false
     config.generators.javascripts = false
+
+    config.exceptions_app = self.routes
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root to: 'top#index'
@@ -27,4 +29,7 @@ Rails.application.routes.draw do
 
   get 'privacy', to: 'pages#privacy'
   get 'terms', to: 'pages#terms'
+
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
