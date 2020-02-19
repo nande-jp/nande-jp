dfe4345c9161d4f8bc694410917e79577168ad89
tmouri <takehiromouri@gmail.com>
2020-02-19 07:34:40 -0800

    Shuffle home feed

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @posts = HomeFeedService.new(user: user_signed_in? ? current_user : nil, page: params.fetch(:page, 1)).get_feed
+    @posts = HomeFeedService.new(user: user_signed_in? ? current_user : nil, page: params.fetch(:page, 1)).get_feed.shuffle
     @popular_questions = Question.order(answers_count: :desc).order('RANDOM()').limit(10)
   end
 end
