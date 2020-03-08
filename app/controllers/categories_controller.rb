b6c44186c05268587875e29a5591869ee310e542
tmouri <takehiromouri@gmail.com>
2020-03-07 21:16:16 -0800

    Fix code

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 class CategoriesController < ApplicationController
   def show
-    @questions = Question.where(category: params[:id]).order(created_at: :desc).paginate(page: params[:page])
+    @answers = Answer.joins(:question)
+                     .where(questions: {category: params[:id]})
+                     .order(created_at: :desc)
+                     .paginate(page: params[:page])
 
     @popular_questions = Question.where(category: params[:id]).order(answers_count: :desc).limit(10)
   end
