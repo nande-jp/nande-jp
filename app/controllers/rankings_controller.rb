b5d9b3a55edf6d1ee270c95ae5d78f7117183fdc
tmouri <takehiromouri@gmail.com>
2020-03-28 16:03:59 -0700

    Fix ranking

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,7 +2,7 @@ class RankingsController < ApplicationController
   add_breadcrumb "ホーム", :root_path
 
   def index
-    @answers = Answer.order(:bookmarks_count, :shares_count).paginate(page: params[:page])
+    @answers = Answer.order(:bookmarks_count, :desc).paginate(page: params[:page])
 
     @popular_questions = Question.order(answers_count: :desc).limit(10)
 
